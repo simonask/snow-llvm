3cc2fccafd1597bec426a46e89092d695a9e7d46
Simon Ask Ulsnes <simon@ulsnes.dk>
2010-11-05 08:47:31 +0100

    Snow internal calling convention, runtime support.

--- snow/include/snow/function.h
+++ snow/include/snow/function.h
@@ -19,15 +19,16 @@ typedef struct SnFunctionSignature {
 	size_t num_params;
 	SnType* param_types;
 	SnSymbol* param_names;
+	int it_index; // index for the `it` argument -- necessary because param_names 
 } SnFunctionSignature;
 
 typedef struct SnFunctionDescriptor {
-	int64_t ref_count;
 	void* jit_handle;
 	SnFunctionSignature* signature;
 	SnFunctionPtr ptr;
 	SnSymbol* local_names;
 	uint32_t num_locals;
+	bool needs_context;
 } SnFunctionDescriptor;
 
 typedef struct SnFunctionCallContext {
@@ -35,8 +36,22 @@ typedef struct SnFunctionCallContext {
 	struct SnFunction* function;
 	struct SnFunctionCallContext* caller;
 	VALUE self;
-	struct SnArguments* arguments;
-	VALUE* locals;
+	
+	/*
+		Organization of the `locals` array:
+		-->    function arguments, in named order
+		       ...
+		       locals in the function
+		       ...
+		       extra named function arguments, in named order
+		       ...
+		       extra unnamed function arguments, in provided order
+	*/
+	VALUE* locals; // size: num_locals (including num_params) + num_extra_named_args + num_extra_unnamed_args
+
+	size_t num_extra_names;
+	size_t num_extra_args; // num_extra_args >= num_extra_names
+	SnSymbol* extra_names; // names for named arguments that aren't specified in the function signature
 } SnFunctionCallContext;
 
 typedef struct SnFunction {
@@ -46,7 +61,9 @@ typedef struct SnFunction {
 } SnFunction;
 
 CAPI SnFunction* snow_create_function(SnFunctionDescriptor* descriptor);
+CAPI SnFunctionCallContext* snow_create_function_call_context(SnFunction* callee, SnFunctionCallContext* caller, size_t num_names, SnSymbol* names, size_t num_args, VALUE* args);
+CAPI SnFunction* snow_value_to_function(VALUE val);
 
-CAPI VALUE snow_function_call(SnFunction* function, VALUE self, struct SnArguments* args);
+CAPI VALUE snow_function_call(SnFunction* function, SnFunctionCallContext* context, VALUE self, VALUE it);
 
 #endif /* end of include guard: FUNCTION_H_X576C5TP */
--- snow/runtime/function.c
+++ snow/runtime/function.c
@@ -11,10 +11,32 @@ SnFunction* snow_create_function(SnFunctionDescriptor* descriptor) {
 	return obj;
 }
 
+SnFunctionCallContext* snow_create_function_call_context(SnFunction* callee, SnFunctionCallContext* caller, size_t num_names, SnSymbol* names, size_t num_args, VALUE* args) {
+	if (!callee->descriptor->needs_context) return caller;
+	
+	SnFunctionCallContext* context = (SnFunctionCallContext*)snow_gc_alloc_object(SnFunctionCallContextType);
+	context->function = callee;
+	context->caller = caller;
+	// TODO!
+	return context;
+}
+
+SnFunction* snow_value_to_function(VALUE val) {
+	// TODO: Functor support
+	ASSERT(snow_type_of(val) == SnFunctionType);
+	return (SnFunction*)val;
+}
+
 void snow_finalize_function(SnFunction* func) {
 }
 
-VALUE snow_function_call(SnFunction* func, VALUE self, struct SnArguments* args) {
-	// TODO
-	return SN_NIL;
+void snow_finalize_function_call_context(SnFunctionCallContext* context) {
+	free(context->locals);
+}
+
+VALUE snow_function_call(SnFunction* function, SnFunctionCallContext* context, VALUE self, VALUE it) {
+	if (!function->descriptor->ptr) {
+		snow_vm_realize_function(function->descriptor);
+	}
+	return function->descriptor->ptr(context, self, it);
 }
