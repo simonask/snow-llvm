959d66e6c05f964b0a0b3c7b38c64ecb9d73a598
Simon Ask Ulsnes <simon@ulsnes.dk>
2010-11-06 20:01:28 +0100

    Fixed bug in snow_create_function_call_context.

--- snow/runtime/function.c
+++ snow/runtime/function.c
@@ -13,7 +13,7 @@ SnFunction* snow_create_function(const SnFunctionDescriptor* descriptor, SnFunct
 }
 
 SnFunctionCallContext* snow_create_function_call_context(SnFunction* callee, SnFunctionCallContext* caller, size_t num_names, SnSymbol* names, size_t num_args, VALUE* args) {
-	if (!callee->descriptor->needs_context) return caller;
+	if (!callee->descriptor->needs_context) return callee->definition_context;
 	
 	SnFunctionCallContext* context = (SnFunctionCallContext*)snow_gc_alloc_object(SnFunctionCallContextType);
 	context->function = callee;
