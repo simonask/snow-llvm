92cf413db229092ec4347769ffb466a323434267
Simon Ask Ulsnes <simon@ulsnes.dk>
2010-11-13 20:47:55 +0100

    Implemented function argument dispatcher.

--- snow/include/snow/function.h
+++ snow/include/snow/function.h
@@ -26,29 +26,25 @@ typedef struct SnFunctionDescriptor {
 	bool needs_context;
 } SnFunctionDescriptor;
 
+struct SnFunctionCallContextExtraArguments;
+
 typedef struct SnFunctionCallContext {
 	// XXX: Order matters -- codegen depends on this specific ordering of members.
 	SnObjectBase base;
 	struct SnFunction* function;
 	struct SnFunctionCallContext* caller;
 	VALUE self;
-	
-	/*
-		Organization of the `locals` array:
-		-->    function arguments, in named order
-		       ...
-		       locals in the function
-		       ...
-		       extra named function arguments, in named order
-		       ...
-		       extra unnamed function arguments, in provided order
-	*/
-	VALUE* locals; // size: num_locals (including num_params) + num_extra_named_args + num_extra_unnamed_args
-
-	size_t num_extra_names;
-	size_t num_extra_args; // num_extra_args >= num_extra_names
-	SnSymbol* extra_names; // names for named arguments that aren't specified in the function signature
+	VALUE* locals;  // locals in the function, including named args. size: descriptor.num_locals - descriptor.num_params
+	struct SnFunctionCallContextExtraArguments* extra; // arguments beyond those already named in the function definition
 } SnFunctionCallContext;
+
+typedef struct SnFunctionCallContextExtraArguments {
+	size_t num_names;
+	size_t num_args; // num_args >= num_names
+	SnSymbol* names; // names for named arguments that aren't specified in the function signature
+	VALUE* named_args;
+	VALUE* unnamed_args;
+} SnFunctionCallContextExtraArguments;
 
 typedef struct SnFunction {
 	SnObjectBase base;
@@ -63,7 +59,7 @@ CAPI SnFunction* snow_value_to_function(VALUE val);
 CAPI VALUE snow_function_call(SnFunction* function, SnFunctionCallContext* context, VALUE self, VALUE it);
 
 // Convenience for C bindings
-CAPI SnFunction* snow_create_method(SnFunctionPtr function, size_t num_args);
+CAPI SnFunction* snow_create_method(SnFunctionPtr function, int num_args);
 
 #define SN_DEFINE_METHOD(PROTO, NAME, PTR, NUM_ARGS) snow_object_set_member(PROTO, PROTO, snow_sym(NAME), snow_create_method(PTR, NUM_ARGS))
 
--- snow/runtime/function.c
+++ snow/runtime/function.c
@@ -6,6 +6,8 @@
 #include "snow/function.h"
 #include "snow/str.h"
 
+#include <string.h>
+
 SnFunction* snow_create_function(const SnFunctionDescriptor* descriptor, SnFunctionCallContext* definition_context) {
 	SnFunction* obj = SN_GC_ALLOC_OBJECT(SnFunction);
 	obj->descriptor = descriptor;
@@ -13,13 +15,111 @@ SnFunction* snow_create_function(const SnFunctionDescriptor* descriptor, SnFunct
 	return obj;
 }
 
+static void ensure_extra_args(SnFunctionCallContext* context, size_t max_num_names, VALUE* named_args) {
+	if (!context->extra) {
+		context->extra = (SnFunctionCallContextExtraArguments*)malloc(sizeof(SnFunctionCallContextExtraArguments));
+		context->extra->names = max_num_names ? (SnSymbol*)malloc(sizeof(SnSymbol)*max_num_names) : NULL; // heuristic maximum
+		context->extra->named_args = named_args;
+		context->extra->unnamed_args = NULL; // unknowable at this point
+		context->extra->num_names = context->extra->num_args = 0; // unknowable at this point
+	}
+}
+
 SnFunctionCallContext* snow_create_function_call_context(SnFunction* callee, SnFunctionCallContext* caller, size_t num_names, const SnSymbol* names, size_t num_args, const VALUE* args) {
+	ASSERT(num_names <= num_args);
 	if (!callee->descriptor->needs_context) return callee->definition_context;
 	
 	SnFunctionCallContext* context = SN_GC_ALLOC_OBJECT(SnFunctionCallContext);
 	context->function = callee;
 	context->caller = caller;
-	// TODO! Set arguments etc.
+	context->self = NULL; // set in snow_function_call
+	context->extra = NULL;
+	
+	const SnFunctionDescriptor* descriptor = callee->descriptor;
+	
+	size_t num_params = descriptor->num_params;
+	
+	// allocate space for more locals than needed, to avoid multiple allocations and ensure linear runtime
+	// TODO: Improve this heuristic.
+	size_t max_num_locals = descriptor->num_locals + num_args;
+	
+	VALUE* locals = (VALUE*)malloc(sizeof(VALUE) * max_num_locals);
+	memset(locals, 0, sizeof(VALUE) * max_num_locals);
+	context->locals = locals;
+	
+	VALUE* extra_named_args = context->locals + descriptor->num_locals;
+	// extra_unnamed_args depends on the number of extra named args, which we don't know yet
+	
+	size_t num_extra_names = 0;
+	size_t num_extra_args = 0;
+	size_t first_unnamed_arg = num_names;
+	
+	// first, put named args in place
+	// INFO: The following code is why param names and named arguments are sorted by symbol value.
+	size_t param_i = 0;
+	size_t arg_i = 0;
+	while (param_i < descriptor->num_params) {
+		bool found = false;
+		while (arg_i < num_names) {
+			if (names[arg_i] == descriptor->param_names[param_i]) {
+				found = true;
+				locals[param_i] = args[arg_i];
+				++arg_i;
+				break;
+			} else {
+				// named argument, but not part of the function definition.
+				// Add it to the extra args.
+				ensure_extra_args(context, num_names, extra_named_args);
+				
+				extra_named_args[num_extra_names] = args[arg_i];
+				context->extra->names[num_extra_names] = names[arg_i];
+				++num_extra_names;
+				++num_extra_args;
+				++arg_i;
+			}
+		}
+		
+		if (!found) {
+			// take an unnamed arg
+			locals[param_i] = args[arg_i];
+			++arg_i;
+		}
+		
+		++param_i;
+	}
+	
+	// Extra named args
+	if (arg_i < num_args)
+		ensure_extra_args(context, num_names, extra_named_args);
+	while (arg_i < num_names) {
+		context->extra->names[num_extra_names] = names[arg_i];
+		context->extra->named_args[num_extra_args] = args[arg_i];
+		++arg_i;
+		++num_extra_names;
+		++num_extra_args;
+	}
+	// Extra unnamed args
+	if (arg_i < num_args) {
+		context->extra->num_names = num_extra_names;
+		context->extra->unnamed_args = context->extra->named_args + num_extra_names;
+	}
+	while (arg_i < num_args) {
+		context->extra->unnamed_args[num_extra_args++] = args[arg_i++];
+		context->extra->num_args = num_extra_args;
+	}
+	
+	// PHEW! The worst is over.
+	
+	/*printf("locals array:\n");
+	for (size_t i = 0; i < max_num_locals; ++i) {
+		if (i < descriptor->num_params) {
+			printf("%s\t", snow_sym_to_cstr(descriptor->param_names[i]));
+		} else {
+			printf("\t\t");
+		}
+		printf("%lu:\t%p\n", i, context->locals[i]);
+	}*/
+	
 	return context;
 }
 
@@ -34,9 +134,20 @@ void snow_finalize_function(SnFunction* func) {
 
 void snow_finalize_function_call_context(SnFunctionCallContext* context) {
 	free(context->locals);
+	if (context->extra) {
+		free(context->extra->names);
+		free(context->extra);
+	}
 }
 
 VALUE snow_function_call(SnFunction* function, SnFunctionCallContext* context, VALUE self, VALUE it) {
+	//printf("snow_function_call(function: %p(%s), context: %p, self: %p, it: %p)\n", function, "<unnamed>"/*snow_string_cstr(snow_vm_get_name_of(function->descriptor->ptr))*/, context, self, it);
+	if (!self)
+		self = function->definition_context ? function->definition_context->self : NULL;
+	if (function->descriptor->needs_context) {
+		// context is owned by function
+		context->self = self;
+	}
 	return function->descriptor->ptr(context, self, it);
 }
 
