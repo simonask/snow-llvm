bac13b62d518f863ec55d1d155a952f05f988d67
Simon Ask Ulsnes <simon@ulsnes.dk>
2011-01-01 13:28:33 +0100

    Object properties

--- snow/include/snow/object.h
+++ snow/include/snow/object.h
@@ -16,18 +16,30 @@ typedef struct SnObjectBase {
 	};
 } SnObjectBase;
 
+typedef struct SnProperty {
+	SnSymbol name;
+	VALUE getter;
+	VALUE setter;
+} SnProperty;
+
 typedef struct SnObject {
 	SnObjectBase base;
 	struct SnObject* prototype;
 	struct SnMap* members;
-	struct SnMap* properties;
+	SnProperty* properties;
+	size_t num_properties;
 	struct SnArray* included_modules;
 } SnObject;
 
 CAPI SnObject* snow_create_object(SnObject* prototype);
+CAPI void snow_finalize_object(SnObject* obj);
 CAPI VALUE snow_object_get_member(SnObject* object, VALUE self_for_properties, SnSymbol member);
 CAPI VALUE snow_object_set_member(SnObject* object, VALUE self_for_properties, SnSymbol member, VALUE val);
 CAPI void snow_object_set_property_getter(SnObject* object, SnSymbol member, VALUE getter);
 CAPI void snow_object_set_property_setter(SnObject* object, SnSymbol member, VALUE setter);
+CAPI void snow_object_define_property(SnObject* object, SnSymbol name, VALUE getter, VALUE setter);
+CAPI bool snow_include_module(SnObject* object, SnObject* module);
+
+#define SN_DEFINE_PROPERTY(OBJECT, NAME, GETTER, SETTER) snow_object_define_property(OBJECT, snow_sym(NAME), snow_create_method(GETTER, 0), snow_create_method(SETTER, 1))
 
 #endif /* end of include guard: OBJECT_H_CCPHDYB5 */
--- snow/runtime/object.c
+++ snow/runtime/object.c
@@ -7,16 +7,26 @@
 #include "snow/exception.h"
 #include "snow/function.h"
 #include "snow/str.h"
+#include "snow/snow.h"
 
-struct SnArguments;
+static int compare_properties(const void* _a, const void* _b) {
+	const SnProperty* a = (const SnProperty*)_a;
+	const SnProperty* b = (const SnProperty*)_b;
+	return a->name - b->name;
+}
 
 SnObject* snow_create_object(SnObject* prototype) {
 	SnObject* obj = SN_GC_ALLOC_OBJECT(SnObject);
 	obj->prototype = prototype;
 	obj->members = NULL;
 	obj->properties = NULL;
+	obj->num_properties = 0;
 	obj->included_modules = NULL;
 	return obj;
+}
+
+void snow_finalize_object(SnObject* obj) {
+	free(obj->properties);
 }
 
 VALUE snow_object_get_member(SnObject* obj, VALUE self, SnSymbol member) {
@@ -26,6 +36,20 @@ VALUE snow_object_get_member(SnObject* obj, VALUE self, SnSymbol member) {
 		if (obj->members) {
 			if ((v = snow_map_get(obj->members, vmember)))
 				return v;
+		}
+		
+		if (obj->properties) {
+			SnProperty key = { member, NULL, NULL };
+			SnProperty* property = (SnProperty*)bsearch(&key, obj->properties, obj->num_properties, sizeof(SnProperty), compare_properties);
+			if (property) {
+				if (property->getter) {
+					return snow_call(property->getter, self, 0, NULL);
+				} else {
+					// TODO: Exception
+					fprintf(stderr, "ERROR: Property '%s' is write-only on object %p (self %p).\n", snow_sym_to_cstr(member), obj, self);
+					return NULL;
+				}
+			}
 		}
 		
 		if (obj->included_modules) {
@@ -42,15 +66,64 @@ VALUE snow_object_get_member(SnObject* obj, VALUE self, SnSymbol member) {
 }
 
 VALUE snow_object_set_member(SnObject* obj, VALUE self, SnSymbol member, VALUE value) {
-	if (obj != self) {
-		snow_throw_exception_with_description("Trying to set member of faux object (properties not supported yet).");
-		return NULL;
+	// First, look for properties. TODO: Find a way to do this faster, perhaps?
+	SnObject* pobj = obj;
+	SnProperty key = { member, NULL, NULL };
+	while (pobj) {
+		if (pobj->properties) {
+			SnProperty* property = (SnProperty*)bsearch(&key, pobj->properties, pobj->num_properties, sizeof(SnProperty), compare_properties);
+			if (property) {
+				if (property->setter) {
+					return snow_call(property->setter, self, 1, &value);
+				} else {
+					// TODO: Exception
+					fprintf(stderr, "ERROR: Property '%s' is read-only on object %p (self %p).\n", snow_sym_to_cstr(member), pobj, self);
+					return NULL;
+				}
+			}
+		}
+		pobj = pobj->prototype;
 	}
+	
+	// TODO: Look for properties in included modules
 	
 	if (!obj->members)
 		obj->members = snow_create_map_with_immediate_keys();
 	snow_map_set(obj->members, snow_symbol_to_value(member), value);
 	return value;
+}
+
+
+void snow_object_define_property(SnObject* obj, SnSymbol name, VALUE getter, VALUE setter) {
+	SnProperty key = { name, NULL, NULL };
+	SnProperty* existing_property = obj->num_properties ? (SnProperty*)bsearch(&key, obj->properties, obj->num_properties, sizeof(SnProperty), compare_properties) : 0;
+	if (existing_property) {
+		existing_property->getter = getter;
+		existing_property->setter = setter;
+	} else {
+		obj->num_properties += 1;
+		obj->properties = (SnProperty*)realloc(obj->properties, (obj->num_properties)*sizeof(SnProperty));
+		size_t insertion_point = 0;
+		while (insertion_point < obj->num_properties-1 && obj->properties[insertion_point].name < name) {
+			++insertion_point;
+		}
+		memmove(obj->properties + insertion_point + 1, obj->properties + insertion_point, obj->num_properties - insertion_point - 1);
+		existing_property = obj->properties + insertion_point;
+		existing_property->name = name;
+		existing_property->getter = getter;
+		existing_property->setter = setter;
+	}
+}
+
+bool snow_object_include_module(SnObject* object, SnObject* module) {
+	if (!object->included_modules) {
+		object->included_modules = snow_create_array();
+	}
+	if (snow_array_contains(object->included_modules, module)) return false;
+	
+	snow_array_push(object->included_modules, module);
+	// TODO: Call module.included(object)
+	return true;
 }
 
 
