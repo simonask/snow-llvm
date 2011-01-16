58c783ca1bdeb20286fbe8d1b9cd57ff5060b194
Simon Ask Ulsnes <simon@ulsnes.dk>
2011-01-16 19:11:39 +0100

    Explanatory comment.

--- snow/runtime/inlined.c
+++ snow/runtime/inlined.c
@@ -1,3 +1,8 @@
+/*
+	This file makes sure that functions that are marked as
+	"inline" in the runtime will still be accessible from outside modules.
+*/
+
 #define inline 
 
 #include "snow/value.h"
