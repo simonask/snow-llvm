359b34f0980282758a736c2b3c7c72c9407dc733
Simon Ask Ulsnes <simon@ulsnes.dk>
2011-03-18 13:30:17 +0100

    Added Boolean#~ (complement).

--- snow/runtime/boolean.c
+++ snow/runtime/boolean.c
@@ -7,8 +7,13 @@ static VALUE boolean_inspect(SnFunctionCallContext* here, VALUE self, VALUE it) 
 	return snow_create_string_constant(snow_value_to_boolean(self) ? "true" : "false");
 }
 
+static VALUE boolean_complement(SnFunctionCallContext* here, VALUE self, VALUE it) {
+	return snow_boolean_to_value(!snow_value_to_boolean(self));
+}
+
 SnObject* snow_create_boolean_prototype() {
 	SnObject* proto = snow_create_object(NULL);
+	SN_DEFINE_METHOD(proto, "~", boolean_complement, 0);
 	SN_DEFINE_METHOD(proto, "inspect", boolean_inspect, 0);
 	SN_DEFINE_METHOD(proto, "to_string", boolean_inspect, 0);
 	return proto;
