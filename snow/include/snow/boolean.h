6a8594478e822f083ab32782b10768c507a2b26e
Simon Ask Ulsnes <simon@ulsnes.dk>
2011-03-18 13:29:41 +0100

    snow_value_to_boolean is now equivalent to snow_eval_truth.

--- snow/include/snow/boolean.h
+++ snow/include/snow/boolean.h
@@ -5,7 +5,7 @@
 #include "snow/basic.h"
 #include "snow/value.h"
 
-INLINE bool snow_value_to_boolean(VALUE val) { return val == SN_TRUE; }
+INLINE bool snow_value_to_boolean(VALUE val) { return snow_eval_truth(val); }
 INLINE VALUE snow_boolean_to_value(bool b) { return b ? SN_TRUE : SN_FALSE; }
 
 #endif /* end of include guard: BOOLEAN_H_L8LG419 */
