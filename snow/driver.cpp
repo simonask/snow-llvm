869cb2c838ab02909d992a72b31f8826ab79c50c
Simon Ask Ulsnes <simon@ulsnes.dk>
2011-03-21 16:47:26 +0100

    Catch Snow exceptions at the top level driver.

--- snow/driver.cpp
+++ snow/driver.cpp
@@ -1,10 +1,23 @@
 #include "libsnow/libsnow.hpp"
+#include <stdlib.h>
+#include <stdio.h>
+#include "snow/exception.hpp"
 
 // TODO: Set up runtime paths etc.
 
 int main (int argc, char const *argv[]) {
-	snow::init("runtime/snow-runtime.bc");
-	int n = snow::main(argc, argv);
-	snow::finish();
-	return n;
+	try {
+		snow::init("runtime/snow-runtime.bc");
+		int n = snow::main(argc, argv);
+		snow::finish();
+		return n;
+	}
+	catch (const snow::Exception& ex) {
+		fprintf(stderr, "Caught Snow exception %p\n", ex.value);
+		abort();
+	}
+	catch (...) {
+		fprintf(stderr, "Unknown exception caught, aborting!\n");
+		abort();
+	}
 }
