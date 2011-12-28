9a4753c3dcae8c6634620811e5c4203011263957
Simon Ask Ulsnes <simon@ulsnes.dk>
2011-12-28 11:07:46 +0100

    Missing header.

---
+++ snow/runtime/gc-intern.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#ifndef GC_INTERN_HPP_ARWL9D38
+#define GC_INTERN_HPP_ARWL9D38
+
+namespace snow {
+	void gc_scan_fiber_stack(byte* top, byte* bottom) {}
+}
+
+#endif /* end of include guard: GC_INTERN_HPP_ARWL9D38 */
