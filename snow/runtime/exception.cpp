bde8c77e09a975d5576abb319004c54f9c612ad1
Simon Ask Ulsnes <simon@ulsnes.dk>
2012-02-17 18:37:31 +0100

    display_stack_trace for debugging exception unwinding.

--- snow/runtime/exception.cpp
+++ snow/runtime/exception.cpp
@@ -3,6 +3,7 @@
 #include "snow/snow.hpp"
 #include <stdio.h>
 #include <stdlib.h>
+#include <libunwind.h>
 
 namespace snow {
 	namespace {
@@ -29,6 +30,25 @@ namespace snow {
 		}
 		return result;
 	}
+	
+	void display_stack_trace(int skip) {
+		unw_cursor_t cursor;
+		unw_context_t uc;
+		unw_word_t ip;
+		unw_word_t sp;
+		
+		unw_getcontext(&uc);
+		unw_init_local(&cursor, &uc);
+		int n = -1; // this function is also counted!
+		while (unw_step(&cursor) > 0) {
+			unw_get_reg(&cursor, UNW_REG_IP, &ip);
+			unw_get_reg(&cursor, UNW_REG_SP, &sp);
+			++n;
+			if (n < skip) continue;
+			printf("ip = %lx, sp = %lx\n", (long)ip, (long)sp);
+		}
+		printf("ip = %lx\n", (long)ip);
+	}
 
 	void throw_exception(Value ex) {
 		// TODO: Backtrace and all that jazz
@@ -42,6 +62,9 @@ namespace snow {
 		#if defined(DEBUG)
 		fprintf(stderr, "SNOW: Throwing exception: %s\n", buffer);
 		#endif
+		
+		//display_stack_trace(1);
+		
 		throw snow::Exception(ex);
 	}
 }
