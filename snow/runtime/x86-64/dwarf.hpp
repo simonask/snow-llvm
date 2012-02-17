e03c6e565061d578e532a0552bec48dbdb9267bc
Simon Ask Ulsnes <simon@ulsnes.dk>
2012-02-17 18:38:46 +0100

    x86-64/dwarf.hpp: constants

--- snow/runtime/x86-64/dwarf.hpp
+++ snow/runtime/x86-64/dwarf.hpp
@@ -2,6 +2,7 @@
 #ifndef DWARF_HPP_FZN5FJAH
 #define DWARF_HPP_FZN5FJAH
 
+#include "../internal.h"
 #include "asm.hpp"
 
 namespace snow {
@@ -9,13 +10,27 @@ namespace snow {
 		using namespace x86_64;
 		
 		static const byte DW_CIE_VERSION   = 0x1;
-		static const uint32_t DW_CIE_ID    = 0xffffffff;
-		static const byte DW_EH_PE_omit    = 0xff;
-		static const byte DW_EH_PE_absptr  = 0x00;
-		static const byte DW_EH_PE_sdata4  = 0x0b;
-		static const byte DW_EH_PE_pcrel   = 0x10;
-		static const byte DW_EH_PE_datarel = 0x30;
-		static const byte DW_EH_PE_udata4  = 0x03;
+		static const int32_t DW_CIE_ID    = 0x0;
+
+		enum DW_EH_PE {
+			DW_EH_PE_absptr = 0x00,
+			DW_EH_PE_omit = 0xff,
+			DW_EH_PE_uleb128 = 0x01,
+			DW_EH_PE_udata2 = 0x02,
+			DW_EH_PE_udata4 = 0x03,
+			DW_EH_PE_udata8 = 0x04,
+			DW_EH_PE_sleb128 = 0x09,
+			DW_EH_PE_sdata2 = 0x0A,
+			DW_EH_PE_sdata4 = 0x0B,
+			DW_EH_PE_sdata8 = 0x0C,
+			DW_EH_PE_signed = 0x08,
+			DW_EH_PE_pcrel = 0x10,
+			DW_EH_PE_textrel = 0x20,
+			DW_EH_PE_datarel = 0x30,
+			DW_EH_PE_funcrel = 0x40,
+			DW_EH_PE_aligned = 0x50,
+			DW_EH_PE_indirect = 0x80,
+		};
 		
 		enum DW_CFA {
 			DW_CFA_extended    = 0x00,
@@ -52,7 +67,7 @@ namespace snow {
 			DW_CFA_hi_user = 0x3f,
 		};
 		
-		byte reg(Register r) {
+		inline byte reg(Register r) {
 			static Register regs_in_dwarf_order[] = { RAX, RDX, RCX, RBX, RSI, RDI, RBP, RSP, R8, R9, R10, R11, R12, R13, R14, R15, RIP };
 			for (size_t i = 0; i < countof(regs_in_dwarf_order); ++i) {
 				if (regs_in_dwarf_order[i] == r)
